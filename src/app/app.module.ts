2a25eb9436faa841d10829a7406406c617210329
paulocayres <paulo.cayres@gmail.com>
2017-10-26 23:06:04 -0200

    Conectado ao firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,19 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAdQrvJ9-OoSoeRaqoTiOc6K_tVvQeEnH0",
+  authDomain: "pccr-login.firebaseapp.com",
+  databaseURL: "https://pccr-login.firebaseio.com",
+  projectId: "pccr-login",
+  storageBucket: "pccr-login.appspot.com",
+  messagingSenderId: "103709366854"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -28,6 +44,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: Observable<any[]>;
 
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase) {
+    this.items = afDB.list('users').valueChanges();
   }
 
 }
